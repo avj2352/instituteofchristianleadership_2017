9b9c6a28115c733d19a93632de668e171e105467
Pramod Jingade <Pramod Jingade>
2017-06-25 20:08:59 +0530

    Completed homepage design

--- public/assets/js/now-ui-kit.js
+++ public/assets/js/now-ui-kit.js
@@ -98,6 +98,15 @@ $(document).ready(function(){
             });
     });
 
+    nowuiKit.initSliders();
+    function scrollToDownload() {
+
+        if ($('.section-download').length != 0) {
+            $("html, body").animate({
+                scrollTop: $('.section-download').offset().top
+            }, 1000);
+        }
+    }
 
 });
 
